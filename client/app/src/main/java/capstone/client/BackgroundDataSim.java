e3b34cdbfcfcb15435a7f02749e14c155991ac75
Grace Cochrane <grace.cochrane@mail.utoronto.ca>
2017-01-08 21:44:12 +0000

    Non working remote server access code

--- client/app/src/main/java/capstone/client/BackgroundDataSim.java
+++ client/app/src/main/java/capstone/client/BackgroundDataSim.java
@@ -2,19 +2,36 @@ package capstone.client;
 
 import android.app.Service;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.IBinder;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
+import com.couchbase.lite.AsyncTask;
 import com.couchbase.lite.CouchbaseLiteException;
 import com.couchbase.lite.Database;
 import com.couchbase.lite.DatabaseOptions;
 import com.couchbase.lite.DocumentChange;
 import com.couchbase.lite.Manager;
 import com.couchbase.lite.android.AndroidContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by Grace on 2017-01-08.
@@ -31,7 +48,9 @@ public class BackgroundDataSim extends Service {
     public BackgroundDataSim() {
     }
 
-    static public BackgroundDataSim get_backgroundDataSim(){return _backgroundDataSim;}
+    static public BackgroundDataSim get_backgroundDataSim() {
+        return _backgroundDataSim;
+    }
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -44,6 +63,9 @@ public class BackgroundDataSim extends Service {
         openDatabase("development");
         _backgroundDataSim = this;
         broadcaster = LocalBroadcastManager.getInstance(this);
+
+
+
         //sit and spin on remote db.
     }
 
@@ -58,10 +80,10 @@ public class BackgroundDataSim extends Service {
         return Service.START_NOT_STICKY;
     }
 
-    public void notifyUI(Map<String,Object> document) {
+    public void notifyUI(Map<String, Object> document) {
         Intent intent = new Intent(DB_UPDATE);
-        for(String key :document.keySet()){
-            intent.putExtra(key,document.get(key).toString());
+        for (String key : document.keySet()) {
+            intent.putExtra(key, document.get(key).toString());
         }
         broadcaster.sendBroadcast(intent);
     }
@@ -83,12 +105,15 @@ public class BackgroundDataSim extends Service {
         database.addChangeListener(new Database.ChangeListener() {
             @Override
             public void changed(Database.ChangeEvent event) {
-                for(DocumentChange dc:event.getChanges()){
-                    Log.i(this.getClass().getSimpleName(), "Document changed: "+ dc.getDocumentId());
+                for (DocumentChange dc : event.getChanges()) {
+                    Log.i(this.getClass().getSimpleName(), "Document changed: " + dc.getDocumentId());
                     notifyUI(database.getDocument(dc.getDocumentId()).getProperties());
                 }
             }
         });
     }
+
+
 }
+
 
