260ada929ba534cb0e11a0aa27bb1d92b62c2b90
Grace Cochrane <grace.cochrane@mail.utoronto.ca>
2017-01-21 19:15:44 +0000

    Added global vars for client app

---
+++ client/app/src/main/java/capstone/client/DRDCClient.java
@@ -0,0 +1,60 @@
+package capstone.client;
+
+import android.app.Application;
+
+import java.util.Date;
+
+/**
+ * Created by Grace on 2017-01-21.
+ */
+
+public class DRDCClient extends Application {
+
+    //global vars for app
+    //access in any activity by String s = ((MyApplication) this.getApplication()).getSomeVariable(); or
+    //    ((MyApplication) this.getApplication()).setSomeVariable("foo");
+    private String soldierID;
+    private String name;
+    private Date dob;
+    private float weight;
+    private float height;
+
+    public String getSoldierID() {
+        return soldierID;
+    }
+
+    public void setSoldierID(String id) {
+        this.soldierID = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float w) {
+        this.weight = w;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float h) {
+        this.height = h;
+    }
+}
