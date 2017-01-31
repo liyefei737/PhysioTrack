305c8c935c0bb9b5ef8b7ee4ffef57790c90c548
mehmetatmaca <mehmet.atmaca@mail.utoronto.ca>
2017-01-31 01:58:09 -0500

    PDA UI Completed

--- client/app/src/main/java/capstone/client/Heart.java
+++ client/app/src/main/java/capstone/client/Heart.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.roughike.bottombar.BottomBar;
@@ -18,6 +20,7 @@ import static capstone.client.R.id.tab_skin;
 
 public class Heart extends AppCompatActivity {
     private TextView messageView;
+    private ImageButton imageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,6 +55,15 @@ public class Heart extends AppCompatActivity {
                     Intent intent = new Intent(Heart.this, Core.class);
                     startActivity(intent);
                 }
+            }
+        });
+        imageButton = (ImageButton)findViewById(R.id.imageButton);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoader = new Intent(Heart.this, Soldier.class);
+                startActivity(intentLoader);
             }
         });
 
--- client/app/src/main/java/capstone/client/Lung.java
+++ client/app/src/main/java/capstone/client/Lung.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.roughike.bottombar.BottomBar;
@@ -17,6 +19,7 @@ import static capstone.client.R.id.tab_skin;
 
 public class Lung extends AppCompatActivity {
     private TextView messageView;
+    private ImageButton imageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,6 +55,15 @@ public class Lung extends AppCompatActivity {
                     Intent intent = new Intent(Lung.this, Core.class);
                     startActivity(intent);
                 }
+            }
+        });
+        imageButton = (ImageButton)findViewById(R.id.imageButtonLung);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoader = new Intent(Lung.this, Soldier.class);
+                startActivity(intentLoader);
             }
         });
 
