191f09d611c1ab800789a3f62fb0d93e999c1f3b
mehmetatmaca <mehmet.atmaca@mail.utoronto.ca>
2017-01-31 01:03:25 -0500

    UI Some update

--- client/app/src/main/java/capstone/client/Soldier.java
+++ client/app/src/main/java/capstone/client/Soldier.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,5 +25,43 @@ public class Soldier extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soldier);
+
+        // Bottom navigation menu
+        messageView =(TextView) findViewById(R.id.messageSoldier);
+        BottomBar bottomBar=(BottomBar) findViewById(R.id.bottomBar);
+
+        bottomBar.setDefaultTab(R.id.tab_home);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                messageView.setText(TabMessage.get(tabId, false));
+                if(tabId==tab_home){
+                    //Toast.makeText(getApplicationContext(), "Heart Rate Page",Toast.LENGTH_LONG).show();
+//                    Intent intent = new Intent(Soldier.this, MainActivity.class);
+//                    startActivity(intent);
+                }
+                if(tabId == tab_lung){
+                    //Toast.makeText(getApplicationContext(), "Skin Temp page",Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(Soldier.this, Lung.class);
+                    startActivity(intent);
+                }
+                if(tabId == tab_heart){
+                    //Toast.makeText(getApplicationContext(), "Skin Temp page",Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(Soldier.this, Heart.class);
+                    startActivity(intent);
+                }
+                if(tabId == tab_core){
+                    //Toast.makeText(getApplicationContext(), "Skin Temp page",Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(Soldier.this, Core.class);
+                    startActivity(intent);
+                }
+                if(tabId == tab_skin){
+                    //Toast.makeText(getApplicationContext(), "Skin Temp page",Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(Soldier.this, Skin.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
     }
 }
