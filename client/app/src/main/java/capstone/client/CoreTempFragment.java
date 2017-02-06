18bdd4a083d2c2fbf1d7a3ed531240e2123e9440
liyefei737 <macrofly@gmail.com>
2017-02-06 02:48:48 -0500

    1.observer pattern to avoid duplicate code in fragments 2.added graphes to each fragment

--- client/app/src/main/java/capstone/client/CoreTempFragment.java
+++ client/app/src/main/java/capstone/client/CoreTempFragment.java
@@ -1,12 +1,24 @@
 package capstone.client;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -18,7 +30,8 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 
-public class CoreTempFragment extends BaseFragment {
+public class CoreTempFragment extends BaseFragment implements DataObserver {
+    private LineChart lineChart;
 
     public static CoreTempFragment  newInstance(int instance) {
         Bundle args = new Bundle();
@@ -32,6 +45,61 @@ public class CoreTempFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_core_temp, container, false);
+        View inflated = inflater.inflate(R.layout.fragment_core_temp, container, false);
+        FragmentDataManager fm = (FragmentDataManager)getActivity();
+        fm.registerFragment(this);
+
+        return inflated;
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        FragmentDataManager fm = (FragmentDataManager)getActivity();
+        fm.unregisterFragment(this);
+        super.onDestroyView();
+    }
+
+    /***
+     *
+     * @param data the update data from from the background
+     */
+    @Override
+    public void update(Map data) {
+        lineChart = (LineChart) getActivity().findViewById(R.id.coreTempChart);
+        float[] skinTemps= (float[]) data.get("coreTemp");
+
+        List<Entry> entries = new ArrayList<Entry>();
+
+        for(int i = 0; i< skinTemps.length; i++){
+            entries.add(new Entry(i, skinTemps[i]));
+        }
+        LineDataSet dataSet = new LineDataSet(entries, "Label"); // add entries to dataset
+        //dataSet.setHighlightEnabled(true);
+        //dataSet.setColor();
+        //dataSet.setValueTextColor(...); // styling, ...
+        LineData lineData = new LineData(dataSet);
+        //dataSet.setDrawFilled(true);
+        //dataSet.setFillDrawable(gradientDrawable);
+        lineChart.setData(lineData);
+        XAxis xAxis = lineChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextSize(14f);
+        xAxis.setTextColor(Color.WHITE);
+        xAxis.setDrawAxisLine(true);
+        xAxis.setDrawGridLines(false);
+        YAxis yAxis = lineChart.getAxisLeft();
+        //yAxis.setTypeface(...);
+        yAxis.setTextSize(14f); // set the text size
+        yAxis.setAxisMinimum(30f); // start at zero
+        yAxis.setAxisMaximum(45f); // the axis maximum is 100
+        yAxis.setTextColor(Color.WHITE);
+        //yAxis.setValueFormatter(new MyValueFormatter());
+        //yAxis.setGranularity(1f); // interval 1
+        yAxis.setLabelCount(5, true); // force 6 labels
+        lineChart.setNoDataText("Loading...");
+        lineChart.invalidate(); // refresh
+
+
     }
 }
--- client/app/src/main/java/capstone/client/HeartFragment.java
+++ client/app/src/main/java/capstone/client/HeartFragment.java
@@ -1,12 +1,23 @@
 package capstone.client;
 
-import android.content.Context;
-import android.net.Uri;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 
 
 /**
@@ -17,13 +28,14 @@ import android.view.ViewGroup;
  * Use the {@link HeartFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HeartFragment extends BaseFragment {
-
+public class HeartFragment extends BaseFragment implements DataObserver {
+    private LineChart lineChart;
     public static HeartFragment newInstance(int instance) {
         Bundle args = new Bundle();
         args.putInt("argsInstance", instance);
         HeartFragment fragment = new HeartFragment();
         fragment.setArguments(args);
+
         return fragment;
     }
 
@@ -31,6 +43,60 @@ public class HeartFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_heart, container, false);
+        View inflated = inflater.inflate(R.layout.fragment_heart, container, false);
+        FragmentDataManager fm = (FragmentDataManager)getActivity();
+        fm.registerFragment(this);
+        return inflated;
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        FragmentDataManager fm = (FragmentDataManager)getActivity();
+        fm.unregisterFragment(this);
+        super.onDestroyView();
+    }
+
+    /***
+     *
+     * @param data the update data from from the background
+     */
+    @Override
+    public void update(Map data) {
+        lineChart = (LineChart) getActivity().findViewById(R.id.heartChart);
+         int[] heartRates= (int[]) data.get("hr");
+
+        List<Entry> entries = new ArrayList<Entry>();
+
+        for(int i = 0; i< heartRates.length; i++){
+            entries.add(new Entry(i, heartRates[i]));
+        }
+        LineDataSet dataSet = new LineDataSet(entries, "Label"); // add entries to dataset
+        //dataSet.setHighlightEnabled(true);
+        //dataSet.setColor();
+        //dataSet.setValueTextColor(...); // styling, ...
+        LineData lineData = new LineData(dataSet);
+        //dataSet.setDrawFilled(true);
+        //dataSet.setFillDrawable(gradientDrawable);
+        lineChart.setData(lineData);
+        XAxis xAxis = lineChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextSize(14f);
+        xAxis.setTextColor(Color.WHITE);
+        xAxis.setDrawAxisLine(true);
+        xAxis.setDrawGridLines(false);
+        YAxis yAxis = lineChart.getAxisLeft();
+        //yAxis.setTypeface(...);
+        yAxis.setTextSize(14f); // set the text size
+        yAxis.setAxisMinimum(0f); // start at zero
+        yAxis.setAxisMaximum(200f); // the axis maximum is 100
+        yAxis.setTextColor(Color.WHITE);
+        //yAxis.setValueFormatter(new MyValueFormatter());
+        //yAxis.setGranularity(1f); // interval 1
+        yAxis.setLabelCount(5, true); // force 6 labels
+        lineChart.setNoDataText("Loading...");
+        lineChart.invalidate(); // refresh
+
+
     }
 }
