3c225024e9c9a52c6bf0375996398e80dfe140be
mehmetatmaca <mehmet.atmaca@mail.utoronto.ca>
2017-02-11 04:00:46 -0500

    small update

---
+++ MedicUI/app/src/main/java/com/drdc/medicui/HelpPage.java
@@ -0,0 +1,23 @@
+package com.drdc.medicui;
+
+/**
+ * Created by mehmetatmaca on 2017-02-11.
+ */
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.support.v4.app.Fragment;
+import android.widget.TextView;
+
+public class HelpPage extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_help_page, container, false);
+        return rootView;
+    }
+}
