28882c39d7e1576124bccd86338e47f7ff659a94
aa3993 <ub@as-MacBook-Air.local>
2017-02-13 05:37:29 -0500

    listview testing

--- MedicUI/app/src/main/java/com/drdc/medicui/NameList.java
+++ MedicUI/app/src/main/java/com/drdc/medicui/NameList.java
@@ -3,6 +3,7 @@ package com.drdc.medicui;
 /**
  * Created by mehmetatmaca on 2017-02-11.
  */
+
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Bundle;
@@ -14,16 +15,22 @@ import android.widget.TextView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import static com.drdc.medicui.R.layout.fragment_name_list;
+
 public class NameList extends Fragment {
 
-    String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry",
-            "WebOS","Ubuntu","Windows7","Max OS X"};
+    String[] mobileArray = {"soldier1", "soldier2", "soldier3", "soldier4",
+            "soldier5", "soldier6", "soldier7", "soldier8"};
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_name_list, container, false);
+//        ArrayAdapter adapter = new ArrayAdapter<String>(fragment_name_list.this,
+//                fragment_name_list, mobileArray);
+//
+
+        View rootView = inflater.inflate(fragment_name_list, container, false);
         return rootView;
     }
 }
