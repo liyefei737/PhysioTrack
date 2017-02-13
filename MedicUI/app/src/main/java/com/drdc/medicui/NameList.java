3cd507c2c83a47ff5ab66654f977b1c18139329b
aa3993 <ub@as-MacBook-Air.local>
2017-02-13 02:53:18 -0500

    medicui implementing team page using listview

--- MedicUI/app/src/main/java/com/drdc/medicui/NameList.java
+++ MedicUI/app/src/main/java/com/drdc/medicui/NameList.java
@@ -6,12 +6,18 @@ package com.drdc.medicui;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
 import android.view.LayoutInflater;
 import android.support.v4.app.Fragment;
 import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class NameList extends Fragment {
 
+    String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry",
+            "WebOS","Ubuntu","Windows7","Max OS X"};
 
 
     @Override
