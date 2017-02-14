f99786c33dedbe88ff057bae1a882d760e1d42bf
mehmetatmaca <mehmet.atmaca@mail.utoronto.ca>
2017-02-14 18:05:30 -0500

    Medic UI merged to the server code

---
+++ server/app/src/main/java/com/drdc1/medic/TreatmentScreenTab.java
@@ -0,0 +1,29 @@
+package com.drdc1.medic;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TreatmentScreenTab extends Fragment {
+
+
+    public TreatmentScreenTab() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_treatment_screen_tab, container, false);
+    }
+
+}
