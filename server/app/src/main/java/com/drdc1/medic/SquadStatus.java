cc30fbcd2e35a37c43cffa305faa7a91e420b96e
liyefei737 <macrofly@gmail.com>
2017-02-19 15:09:15 -0500

    An attemp to to svg for bullseye

--- server/app/src/main/java/com/drdc1/medic/SquadStatus.java
+++ server/app/src/main/java/com/drdc1/medic/SquadStatus.java
@@ -1,12 +1,16 @@
 package com.drdc1.medic;
 
-import android.content.Context;
-import android.net.Uri;
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
+import android.graphics.Color;
+import android.graphics.drawable.VectorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,8 +25,23 @@ public class SquadStatus extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_squad_status, container, false);
+        View view = inflater.inflate(R.layout.fragment_squad_status, container, false);
+        VectorDrawable overallStatusGraph = (VectorDrawable)((ImageView)view.findViewById(R.id.overallStatusGraph)).getDrawable();
+        VectorDrawable skinTmpGraph = (VectorDrawable)((ImageView)view.findViewById(R.id.skinTempGraph)).getDrawable();
+        VectorDrawable coreTmpGraph = (VectorDrawable)((ImageView)view.findViewById(R.id.coreTempGraph)).getDrawable();
+        VectorDrawable fatigueGraph = (VectorDrawable)((ImageView)view.findViewById(R.id.fatiqueLevelGraph)).getDrawable();
+        ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), Color.RED, Color.GREEN);
+        colorAnimation.setDuration(250); // milliseconds
+        colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
+            @Override
+            public void onAnimationUpdate(ValueAnimator animator) {
+                textView.setBackgroundColor((int) animator.getAnimatedValue());
+            }
+
+        });
+        colorAnimation.start();
+        return view;
     }
 
 }
