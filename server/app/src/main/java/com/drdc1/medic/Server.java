33ac4c681d2321dfdd6bd17185dfbf3c878bf624
ub <ardaaktolun@gmail.com>
2017-02-25 22:22:55 -0500

    added handshake function

--- server/app/src/main/java/com/drdc1/medic/Server.java
+++ server/app/src/main/java/com/drdc1/medic/Server.java
@@ -4,6 +4,13 @@ import android.content.Intent;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.Response.ErrorListener;
+import com.android.volley.Response.Listener;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.couchbase.lite.CouchbaseLiteException;
 import com.couchbase.lite.Database;
 import com.couchbase.lite.Document;
@@ -56,64 +63,101 @@ public class Server extends NanoHTTPD {
 
         try {
             final JSONObject body = new JSONObject(jsonStr);
-            if (!connectionlist.containsKey(body.getString("soldierID"))) {
+            String soldierID = body.getString("soldierID");
+
+            if (!connectionlist.containsKey(soldierID)) {
                 connectionlist.put(body.getString("soldierID"),
                         session.getHeaders().get("http-client-ip"));
-            }
-            Log.d("sender", "Broadcasting message");
-            Intent intent = new Intent("custom-event-name");
-            // You can also include some extra data.
-//            intent.putExtra("message", "99999");
-            intent.putExtra("message", body.getString("ECG Heart Rate"));
-            LocalBroadcastManager.getInstance(AppContext.getContext()).sendBroadcast(intent);
+                if (!dataManager.soldierInSystem(soldierID)) {
+                    Map<String, Object> infoMap = null;
+                    infoMap.put("name", body.getString("name"));
+                    infoMap.put("age", body.getString("age"));
+                    infoMap.put("height", body.getString("height"));
+                    infoMap.put("weight", body.getString("weight"));
 
-            String soldierID = body.getString("soldierID");
-            if (!dataManager.soldierInSystem(soldierID)) {
-                Map<String, Object> infoMap = null;
-                infoMap.put("name", body.getString("name"));
-                infoMap.put("age", body.getString("age"));
-                infoMap.put("height", body.getString("height"));
-                infoMap.put("weight", body.getString("weight"));
+                    dataManager.addSoldier(soldierID, infoMap);
 
-                dataManager.addSoldier(soldierID, infoMap);
-            }
+                    String url = "http://" + session.getHeaders().get("http-client-ip");
 
-            Calendar keyCal = new GregorianCalendar();
-            try {
-                keyCal.setTime(keyFormat.parse(body.getString("DateTime")));
-            } catch (Exception e) {
-                keyCal.set(2017, 01, 30);
-            }
-            Calendar nearestMinute =
-                    org.apache.commons.lang3.time.DateUtils.round(keyCal, Calendar.MINUTE);
-
-            Database db = dataManager.getSoldierDB(soldierID);
-            Document doc = db.getDocument(String.valueOf(nearestMinute.getTimeInMillis()));
-
-            doc.update(new Document.DocumentUpdater() {
-                @Override
-                public boolean update(UnsavedRevision newRevision) {
-                    Map<String, Object> properties = newRevision.getUserProperties();
-//                    HashMap<String, Object> hM = null;
-                    String hrate = null;
-                    try {
-                        properties.put("timeCreated", body.getString("DateTime"));
-                        properties.put("accSum", body.getString("accSum"));
-                        properties.put("skinTemp", body.getString("Skin_Temp"));
-                        properties.put("coreTemp", body.getString("Core_Temp"));
-                        properties.put("heartRate", body.getString("ECG Heart Rate"));
-                        hrate = body.getString("ECG Heart Rate");
-                        properties.put("breathRate", body.getString("Belt Breathing Rate"));
-                        properties.put("bodyPosition", body.getString("BodyPosition"));
-                        properties.put("motion", body.getString("Motion"));
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                    newRevision.setUserProperties(properties);
-
-                    return true;
+                    StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                            new com.android.volley.Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+//                                    try {
+//                                        JSONObject jsonResponse =
+//                                                new JSONObject(response).getJSONObject("form");
+//                                        String site = jsonResponse.getString("site"),
+//                                                network = jsonResponse.getString("network");
+//                                        System.out
+//                                                .println("Site: " + site + "\nNetwork: " + network);
+//                                    } catch (JSONException e) {
+//                                        e.printStackTrace();
+//                                    }
+                                }
+                            },
+                            new com.android.volley.Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    error.printStackTrace();
+                                }
+                            }
+                    ) {
+                        @Override
+                        protected Map<String, String> getParams() {
+                            Map<String, String> params = new HashMap<>();
+                            // the POST parameters:
+                            params.put("handshake", "1");
+                            return params;
+                        }
+                    };
+                    Volley.newRequestQueue(AppContext.getContext()).add(postRequest);
                 }
-            });
+            } else {
+                Log.d("sender", "Broadcasting message");
+                Intent intent = new Intent("custom-event-name");
+                // You can also include some extra data.
+//            intent.putExtra("message", "99999");
+                intent.putExtra("message", body.getString("ECG Heart Rate"));
+                LocalBroadcastManager.getInstance(AppContext.getContext()).sendBroadcast(intent);
+
+                Calendar keyCal = new GregorianCalendar();
+                try {
+                    keyCal.setTime(keyFormat.parse(body.getString("DateTime")));
+                } catch (Exception e) {
+                    keyCal.set(2017, 01, 30);
+                }
+                Calendar nearestMinute =
+                        org.apache.commons.lang3.time.DateUtils.round(keyCal, Calendar.MINUTE);
+
+                Database db = dataManager.getSoldierDB(soldierID);
+                Document doc = db.getDocument(String.valueOf(nearestMinute.getTimeInMillis()));
+
+                doc.update(new Document.DocumentUpdater() {
+                    @Override
+                    public boolean update(UnsavedRevision newRevision) {
+                        Map<String, Object> properties = newRevision.getUserProperties();
+//                    HashMap<String, Object> hM = null;
+                        String hrate = null;
+                        try {
+                            properties.put("timeCreated", body.getString("DateTime"));
+                            properties.put("accSum", body.getString("accSum"));
+                            properties.put("skinTemp", body.getString("Skin_Temp"));
+                            properties.put("coreTemp", body.getString("Core_Temp"));
+                            properties.put("heartRate", body.getString("ECG Heart Rate"));
+                            hrate = body.getString("ECG Heart Rate");
+                            properties.put("breathRate", body.getString("Belt Breathing Rate"));
+                            properties.put("bodyPosition", body.getString("BodyPosition"));
+                            properties.put("motion", body.getString("Motion"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        newRevision.setUserProperties(properties);
+
+                        return true;
+                    }
+                });
+
+            }
 
         } catch (JSONException e) {
             e.printStackTrace();
